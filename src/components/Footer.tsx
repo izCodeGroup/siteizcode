f462b73fdc459ac8245e1c85e23153635b6afedd
Gheno <106445969+ghenosec@users.noreply.github.com>
2025-04-13 20:22:34 -0300

    feat: add email for contact

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   
@@ -8,7 +7,7 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-6 md:mb-0">
             <img 
-              src="/public/logo.png" 
+              src="/logo.png" 
               alt="izcode Logo" 
               className="h-8 mb-6"
             />
@@ -21,12 +20,22 @@ const Footer = () => {
           <div className="text-center md:text-right">
             <div className="mb-4">
               <a 
-                href="https://api.whatsapp.com/send/?phone=5531991732236&text&type=phone_number&app_absent=0r" 
+                href="href="https://api.whatsapp.com/send/?phone=5531991732236&text=Ol%C3%A1%2C%20vim%20pelo%20site%2C%20estou%20interessado%20no%20servi%C3%A7o%20da%20izcode&type=phone_number&app_absent=0" 
                 target="_blank" 
                 rel="noreferrer"
                 className="font-mono text-sm uppercase tracking-wider hover:text-izcode-blue transition-colors"
               >
                 WhatsApp: (31) 99173-2236
+              </a>
+            </div>
+            <div className="mb-4">
+              <a
+                href="mailto:contato@izcode.com.br"
+                target="_blank"
+                rel="noreferrer"
+                className="font-mono text-sm uppercase tracking-wider hover:text-izcode-blue transition-colors"
+              >
+                contato@izcode.com.br
               </a>
             </div>
             <p className="text-sm font-light">
